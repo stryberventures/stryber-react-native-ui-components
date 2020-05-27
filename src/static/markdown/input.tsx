658e9eb83c2c5a88e597f91765e3ae8be1819a6e
Pavel Lapin <pavel@stryber.com>
2020-05-27 16:06:05 +0300

    TOUC-28 FE: Update Input Fields - story adjustments

--- src/static/markdown/input.tsx
+++ src/static/markdown/input.tsx
@@ -19,9 +19,6 @@ import {Input} from '@stryberventures/stryber-react-native-ui-components';
 
 | Prop (boolean)                                                   | Description                                                                                                    |
 | ------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------- |
-| **\`email\`**  | Set input prop keyboardType to "email-address"                                                           |
-| **\`phone\`**  | Set input prop keyboardType to "phone-pad"                                           |
-| **\`number\`**  | Set input prop keyboardType to "numeric"                                            |
 | **\`secure\`**  | Set input prop secureTextEntry to "true" and add toggle button                                         |
 | **\`required\`**  | Add * to input label                                                                                    |
 | **\`disabled\`**  | Disable input field. Set editable to false                                                           |
@@ -29,14 +26,22 @@ import {Input} from '@stryberventures/stryber-react-native-ui-components';
 
 | Prop                                                   | Description                                                                                                    |
 | ------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------- |
+| **\`type\`**  | used for defining input keyboardType, can be 'email' | 'phone' | 'number' | 'default'                                                           |
 | **\`value\`**  | Set input value                                                          |
-| **\`name\`**  | Set input field name that will be returned in onPress function argument                                                          |
+| **\`name\`**  | Set input field name that will be returned in onChange function argument                                                          |
 | **\`onChange\`**  | A function that will be executed on value change event and get in params: onChange(value, name)                                           |
-| **\`placeholder\`**  | Add placeholder to input. If you don't set label, label will be same as placeholder                                                             |
+| **\`placeholder\`**  | Add placeholder to input.                                                             |
+| **\`label\`**  | Add label to input.                                                             |
 | **\`error\`**  | Set input error                                                         |
 | **\`onFocus\`**  | A function that will be executed on input focus                                            |
 | **\`onBlur\`**  | A function that will be executed on input blur                                                                                |
-| **\`icon\`**  | A function that should return icon component                                                           |
+| **\`icon\`**  | A function that should return icon or some button component                                                           |
+| **\`rightIcon\`**  | A function that should return icon or some button component                                                           |
 | **\`mask\`**  | Add mask to input. Example: "XX/XX"                                                           |
 | **\`style\`**  | Use to set input styles                                                          |
+| **\`multiline\`** | Makes multiline input |
+| **\`maxLength\`** | Max string length |
+| **\`numberOfLines\`** | Min number of lines in multiline mode |
+| **\`maxNumberOfLines\`** | Number of lines in multiline mode before showing scrollbars |
+| **\`classes\`** | Object with styling classes that can override |
 `;
