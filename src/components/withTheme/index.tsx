91c041107e3936da119d1b26f24eda081cd5402b
Anton Zaporozhets <zaporozhetsanton@gmail.com>
2020-06-09 11:07:02 +0300

    withTheme fix

--- src/components/withTheme/index.tsx
+++ src/components/withTheme/index.tsx
@@ -3,41 +3,39 @@ import {storage} from '../other/core';
 import {ThemeContext} from '../ThemeContextProvider';
 import {THEME_KEY, defaultTheme} from '../other/constants';
 const withTheme = <ComposedComponentProps extends {}>(
-  Component: ComponentClass<ComposedComponentProps>,
+  Component:
+    | React.FC<ComposedComponentProps>
+    | ComponentClass<ComposedComponentProps>,
 ) => {
-  type ComposedComponentInstance = InstanceType<typeof Component>;
-
   type WrapperComponentProps = ComposedComponentProps & {
     themes?: any[];
     theme?: any;
     setTheme?: Function;
   };
 
-  return forwardRef<ComposedComponentInstance, WrapperComponentProps>(
-    (props, ref) => {
-      const {
-        themes = [defaultTheme],
-        themeID = 'Default',
-        setThemeID = () => {},
-      } = useContext(ThemeContext) || {};
-      /* eslint-disable no-shadow */
-      const getTheme = (themeID: string) =>
-        themes.find(theme => theme.key === themeID);
-      const setTheme = (themeID: string) => {
-        storage.saveString(THEME_KEY, themeID);
-        setThemeID(themeID);
-      };
-      /* eslint-enable no-shadow */
-      return (
-        <Component
-          {...props}
-          themes={themes}
-          theme={getTheme(themeID)}
-          setTheme={setTheme}
-          ref={ref}
-        />
-      );
-    },
-  );
+  return forwardRef<React.ReactNode, WrapperComponentProps>((props, ref) => {
+    const {
+      themes = [defaultTheme],
+      themeID = 'Default',
+      setThemeID = () => {},
+    } = useContext(ThemeContext) || {};
+    /* eslint-disable no-shadow */
+    const getTheme = (themeID: string) =>
+      themes.find(theme => theme.key === themeID);
+    const setTheme = (themeID: string) => {
+      storage.saveString(THEME_KEY, themeID);
+      setThemeID(themeID);
+    };
+    /* eslint-enable no-shadow */
+    return (
+      <Component
+        {...props}
+        themes={themes}
+        theme={getTheme(themeID)}
+        setTheme={setTheme}
+        ref={ref}
+      />
+    );
+  });
 };
 export default withTheme;
