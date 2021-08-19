643b89da5b9f64b3cb79028de1e5c9cf8a8867d2
Vlad Beltsar <vladyslavbeltsar@gmail.com>
2021-08-19 18:01:54 +0300

    Updated react native

--- src/storybook/stories/Header.story.tsx
+++ src/storybook/stories/Header.story.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { SafeAreaView } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
 import { storiesOf } from "@storybook/react-native";
 import Header from "../../components/Header";
 import AnimatedHeader from "../../components/AnimatedHeader";
@@ -17,35 +16,6 @@ storiesOf("Header", module)
     return (
       <Header
         text="Test"
-        leftIcon={() => (
-          <Icon
-            style={{
-              height: 24,
-              width: 44,
-              position: "absolute",
-              left: 10,
-              fontSize: 24
-            }}
-            color="#fff"
-            name="ios-arrow-back"
-            onPress={() => {}}
-          />
-        )}
-        rightIcon={() => (
-          <Icon
-            style={{
-              height: 24,
-              width: 44,
-              position: "absolute",
-              right: 10,
-              fontSize: 24,
-              textAlign: "right"
-            }}
-            color="#fff"
-            name="ios-search"
-            onPress={() => {}}
-          />
-        )}
       />
     );
   })
