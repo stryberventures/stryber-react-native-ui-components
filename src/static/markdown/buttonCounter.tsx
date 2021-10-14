6867af17ffeb103e11d923789d9b55c165d0187b
Pavel Lapin <pavel@stryber.com>
2021-10-14 13:08:09 +0300

    Added 'value' property

--- src/static/markdown/buttonCounter.tsx
+++ src/static/markdown/buttonCounter.tsx
@@ -13,12 +13,16 @@ import {ButtonCounter, Text} from '@stryberventures/stryber-react-native-ui-comp
 
 | Prop                                                   | Description                                                                                                    |
 | ------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------- |
-| **\`ononCountChange\`**  | Function that will be executed when user changes count value by tapping on buttons   
+| **\`onCountChange\`**  | Function that will be executed when user changes count value by tapping on buttons   
+| **\`renderCount\`**  | Function that renders custom count 
 | **\`color\`** | Button background color. Default colors - colors from matterhorn styleguide            |
 | **\`shape\`** | String, set shape of button corners. One of: rectangle, rounded, round           |
+| **\`size\`** | String, modifies button to one of views: regular, small, mini         |
 | **\`iconProps\`** | Object, add properties to the button icon.                                          |
 | **\`style\`**  | An object with button styles 
 | **\`shadow\`**      | Boolean, add shadow to button |
-| **\`small\`**      | Boolean, adds styles of small button |
-| **\`mini\`**      | Boolean, adds styles of mini button |
+| **\`value\`**      | Number, count number |
+| **\`initialValue\`**      | Number, value on initialization |
+| **\`minValue\`**      | Number min value |
+| **\`maxValue\`**      | Number, max value |
 `;
