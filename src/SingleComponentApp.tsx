6575ceff857fd6d31137900edf8c86498bcfe442
pavellapin <pavel@stryber.com>
2022-04-04 13:25:32 +0300

    OX-363 FE: RTL Support for form component - fixed typescript errors

--- src/SingleComponentApp.tsx
+++ src/SingleComponentApp.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 
-import Slider from './components/Slider';
+import Input from './components/Input';
 
 export default () => (
   <View
@@ -10,12 +10,6 @@ export default () => (
       marginTop: 200,
       marginHorizontal: 20,
     }}>
-    <Slider
-      valueUp={3}
-      limitUp={5}
-      limitDown={0}
-      step={1}
-      onChange={() => {}}
-    />
+    <Input />
   </View>
 );
