18c4b6b996d863ea3b779dffbfea8a33096caddd
vladbelcar <vladstart1@hotmail.com>
2022-04-11 19:59:29 +0300

    Updated theme docs

--- src/components/Theme/Theme.web.stories.tsx
+++ src/components/Theme/Theme.web.stories.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import ThemeChooser from '../../storybook/preview/ThemeChooser';
+import {theme} from '../../static/markdown';
 
 export default {
   title: 'Theme',
   component: ThemeChooser,
+  parameters: {
+    notes: {theme},
+  },
 };
 
 export const Default = () => <ThemeChooser />;
